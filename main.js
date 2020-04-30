84613a8a3e5d52fe8c7ba534790bf04a05bfce23
Jun Ichikawa <june2416@gmail.com>
2020-05-01 02:47:14 +0900

    rename

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ javascript:(async function () {
     const endDate = 31;
     const startTime = '09:30';
     const endTime = '18:00';
-    const waitMsec = 1000;
+    const waitMSec = 1000;
 
     // weekly report end time
     let dt = new Date(0, 0, 0, Number(endTime.slice(0, 2)), Number(endTime.slice(-2)));
     dt.setMinutes(dt.getMinutes() + 30);
-    const weekly_report_endTime = dt.getHours().toString() + ':' + dt.getMinutes().toString();
+    const weeklyReportEndTime = dt.getHours().toString() + ':' + dt.getMinutes().toString();
 
     for (let i = startDate; i <= endDate; i++) {
         const dt = new Date();
@@ -23,16 +23,16 @@ javascript:(async function () {
             document.getElementById('startTime').value = startTime;
 
             if (isFriday) {
-                document.getElementById('endTime').value = weekly_report_endTime;
+                document.getElementById('endTime').value = weeklyReportEndTime;
                 document.getElementById('startOut1').value = endTime;
-                document.getElementById('endOut1').value = weekly_report_endTime;
+                document.getElementById('endOut1').value = weeklyReportEndTime;
             } else {
                 document.getElementById('endTime').value = endTime;
             }
 
             document.getElementById('dlgInpTimeOk').click();
 
-            await new Promise(resolve => setTimeout(resolve, waitMsec));
+            await new Promise(resolve => setTimeout(resolve, waitMSec));
 
             // input note
             if (isFriday) {
@@ -40,7 +40,7 @@ javascript:(async function () {
                 document.getElementById('dialogNoteText2').value = '週報作成 0:30h';
                 document.getElementById('dialogNoteOk').click();
 
-                await new Promise(resolve => setTimeout(resolve, waitMsec));
+                await new Promise(resolve => setTimeout(resolve, waitMSec));
             }
         }
     }
