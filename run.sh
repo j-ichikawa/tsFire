1a4c105b43f4b94c6c030365fe29879dafc60207
Jun Ichikawa <june2416@gmail.com>
2020-05-27 14:15:28 +0900

    fix

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ fi
 python3 -m venv venv 1> /dev/null
 source venv/bin/activate 1> /dev/null
 pip3 install -r requirements.txt 1> /dev/null
-python3 main.py "$1"
+python3 __main__.py "$1"
 deactivate
